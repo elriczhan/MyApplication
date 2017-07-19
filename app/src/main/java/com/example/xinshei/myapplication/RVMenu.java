ae50e6531d33f25ed4b963df28015393ed600681
Elric <elriczhan@163.com>
2017-07-19 17:33:43 +0800

    2222

--- app/src/main/java/com/example/xinshei/myapplication/RVMenu.java
+++ app/src/main/java/com/example/xinshei/myapplication/RVMenu.java
@@ -33,7 +33,7 @@ public class RVMenu extends AppCompatActivity {
         list.add(FlowerActivity.class);
         list.add(CameraActivity.class);
         list.add(VideoWallPaperActivity.class);
-        //版本管理测试 test123 4 tag2 hahaha
+        //版本管理测试 test123 4 tag2 hahaha22222
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new rvAdapter());
     }
